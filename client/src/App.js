c8a8071cf3cd37ed3146122874ffc53f40f5459f
Skyler <skyler2440@gmail.com>
2019-07-22 16:32:21 -0400

    Added Routes and Link
    Link updates getting cannot get error

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -12,7 +13,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Router>
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route exact path="/movies/:id" component={Movie} />
+        </Switch>
+        </Router>
+      </div>
     </div>
   );
 };
